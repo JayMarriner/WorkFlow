b88b52c0f6d7c7a1d1ce6352e32ae422194b8ca7
Jay Marriner <jaymarriner123@gmail.com>
2021-03-18 01:09:27 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<head>
+  <link rel="stylesheet" href="Style/main.css">
+  <title>WorkFlow</title>
+</head>
+
+<body>
+  <section id='top'>
+    <h1>WorkFlow</h1>
+    <p>A work management tool made by Jay Marriner.</p>
+  </section>
+
+  <main>
+    <section id='mainArea'>
+      <h1>Projects</h1>
+      <a href='#'>
+        <article id='entry'>
+          <img src='Images/rt.png'>
+          <section id='entryInfo'>
+            <h2>RuneTime</h2>
+            <p>RuneTime is a RuneScape fansite with a focus on the community aspect of the game.</p>
+          </section>
+        </article>
+      </a>
+
+    </section>
+  </main>
+
+  <footer>
+  </footer>
+</body>
